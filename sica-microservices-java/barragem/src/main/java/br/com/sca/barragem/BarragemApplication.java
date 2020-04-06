011b144932fefd544e909808aac02f7e916a060e
bacelarnetto <bacelar.netto.analista@gmail.com>
2020-04-06 15:00:26 -0300

    Ajustes no maven client  eureka

--- sica-microservices-java/barragem/src/main/java/br/com/sca/barragem/BarragemApplication.java
+++ sica-microservices-java/barragem/src/main/java/br/com/sca/barragem/BarragemApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan({"br.com.sca.barragem.model"})
 @EnableJpaRepositories({"br.com.sca.barragem.repository"})		// Enable eureka client.
+@EnableEurekaClient
 @EnableCircuitBreaker 	// Enable circuit breakers
 @ComponentScan("br.com.sca")
 public class BarragemApplication {
--- sica-microservices-java/hystrix-dashboard/src/main/java/br/com/sca/hystrix/dashboard/HystrixDashboardApplication.java
+++ sica-microservices-java/hystrix-dashboard/src/main/java/br/com/sca/hystrix/dashboard/HystrixDashboardApplication.java
@@ -3,9 +3,11 @@ package br.com.sca.hystrix.dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @EnableHystrixDashboard
+@EnableEurekaClient
 @SpringBootApplication
 public class HystrixDashboardApplication {
 
