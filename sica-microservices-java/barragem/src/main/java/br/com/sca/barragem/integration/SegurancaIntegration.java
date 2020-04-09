4d8d778d247452789540ff2bdcd0941d05bd0dd0
bacelarnetto <bacelar.netto.analista@gmail.com>
2020-04-08 22:37:14 -0300

    Comunicação entre api de barragem e segurança

--- sica-microservices-java/barragem/src/main/java/br/com/sca/barragem/integration/SegurancaIntegration.java
+++ sica-microservices-java/barragem/src/main/java/br/com/sca/barragem/integration/SegurancaIntegration.java
@@ -3,6 +3,7 @@ package br.com.sca.barragem.integration;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
@@ -12,13 +13,15 @@ import org.springframework.web.client.RestTemplate;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 import br.com.sca.barragem.dto.EnvioAlertaDTO;
+import br.com.sca.barragem.dto.MoradorAlertaDTO;
 import br.com.sca.barragem.model.Morador;
 import br.com.sca.barragem.repository.MoradorRepository;
 
 @Service
 public class SegurancaIntegration {
 
-	private final String baseUrl = "http://localhost:3333/cidades";
+	@Value("${url.api.seguraca.moradores}")
+	private String baseUrl;
 
 	@Autowired
 	private MoradorRepository repo;
@@ -32,7 +35,7 @@ public class SegurancaIntegration {
 
 	@HystrixCommand(fallbackMethod = "fallback")
 	public String enviarAlerta(EnvioAlertaDTO dto) {
-		List<Morador> moradores = repo.findListMoradorByIdBarragem(dto.getIdBarragem());
+		List<MoradorAlertaDTO> moradores = repo.findListMoradorAlertaByIdBarragem(dto.getIdBarragem());
 		restTemplate.postForObject(baseUrl, moradores, ResponseEntity.class);
 		return "OK";
 	}
@@ -44,7 +47,6 @@ public class SegurancaIntegration {
 	}
 
 	@Bean
-	@LoadBalanced
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
