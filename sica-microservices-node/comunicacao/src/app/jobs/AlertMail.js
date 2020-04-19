16581b6bb1f4a3147cf6716482014e159e9b2fcb
bacelarnetto <bacelar.netto.analista@gmail.com>
2020-04-19 00:48:29 -0300

    melhorias nas mensagens de alerta

--- sica-microservices-node/comunicacao/src/app/jobs/AlertMail.js
+++ sica-microservices-node/comunicacao/src/app/jobs/AlertMail.js
@@ -9,8 +9,8 @@ export default {
       from: 'SCA Alerta <queue@queuetest.com.br>',
       to: `${morador.name} <${morador.email}>`,
       subject: `Atenção!! Aviso de rompimento da Barragem ${morador.barragem}!`,
-      html: `Alerta de Perigo!! Por favor, ${morador.name}, evacuar área imediatamente,` + 
-            `a "Barragem ${morador.barragem}" que fica nas proximidades de sua residencia sofreu um rompimento. ` +
+      html: `Alerta de Perigo!! Por favor, ${morador.name}, evacuar área imediatamente. ` + 
+            `A "Barragem ${morador.barragem}" que fica nas proximidades de sua residência sofreu um rompimento. ` +
             'Procurar regiões altas sem risco de sofrer inundação.'
     });
   },
